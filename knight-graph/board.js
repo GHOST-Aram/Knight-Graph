a01c45a8c96a48ccb49b79c210ad2ee50931b453
GHOST-Aram <owinoojala@gmail.com>
2023-04-03 10:14:34 +0300

    feat: Create KnightGraph

--- knight-graph/board.js
+++ knight-graph/board.js
@@ -4,7 +4,7 @@ export class Board{
         let rank = 0
         
         //Represent the whole board of 8 * 8 squares
-        this.squares = []
+        this.ranks = []
         
         //Create nodes in 8 * 8 Array of nodes
         while(rank < 8){//For every rank
@@ -27,11 +27,43 @@ export class Board{
             }
             //Once the rank is full with 8 squares, 
             // push the rank to the sqaures array 8 times creating a full board of 64 squares
-            this.squares.push(rankSquares)
+            this.ranks.push(rankSquares)
 
             //Increment ranks
             rank ++
         }
     }
+
+    //Create night graph: Coonect all sqares on board to all possible knight moves of each
+    createKnightGraph(){
+        this.ranks.forEach(rank => {
+            rank.forEach(sqaureNode =>{
+
+                sqaureNode.topTopLeft = new Node((sqaureNode.file - 1), (sqaureNode.rank + 2))
+
+                sqaureNode.topTopRight = new Node((sqaureNode.file + 1), (sqaureNode.rank + 2))
+                
+                sqaureNode.leftTopLeft = new Node((sqaureNode.file - 2), (sqaureNode.rank + 1))
+
+                sqaureNode.leftBottomLeft = new Node((sqaureNode.file - 2), (sqaureNode.rank - 1))
+                
+                sqaureNode.bottomBottomLeft = new Node((sqaureNode.file + 1), (sqaureNode.rank - 2))
+
+                sqaureNode.bottomBottomRight = new Node((sqaureNode.file + 1), (sqaureNode.rank - 2))
+                
+                sqaureNode.rightBottomRight = new Node((sqaureNode.file + 2), (sqaureNode.rank - 1))
+
+                sqaureNode.rightTopRight = new Node((sqaureNode.file + 2), (sqaureNode.rank + 1))
+
+            })
+        });
+
+        return this.ranks
+    }
 }
+const board = new Board ()
+
+let knightGraph = board.createKnightGraph()
+
+console.log(knightGraph)
 
